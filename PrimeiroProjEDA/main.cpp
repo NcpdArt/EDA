236516c3344c185de7c08f85eb19e993f6b90c32
NcpdArt <128602738+NcpdArt@users.noreply.github.com>
2023-04-04 19:47:47 +0100

    Update main.cpp

--- PrimeiroProjEDA/main.cpp
+++ PrimeiroProjEDA/main.cpp
@@ -18,46 +18,40 @@ string *modelos;
 
 
 
-//CODIGO MAIN AINDA ESTÁ PRECISANDO DE ALTERAÇÕES
-
-// ERRO DOS NOMES DAS MARCAS (A MARCA ESTA PRESO NUMA CONSTANTE PARA TODOS OS ET) NESSE LOOP E ERRO NOS MODELOS(OS MODELOS MUDA DE ET PARA ET, MAS N ESTAO RANDOM ESTAO POR ODEM DO TXT), ERRO QUE QUANDO OS CARROS SAO 0 A MARCA NO ET FICA VAZIA
-
 int main() {
     srand(time(NULL));
-    const int tamanho_marca = contaLinhas("marcas.txt");
+    const int tamanho_marcas = contaLinhas("marcas.txt");
     const int tamanho_modelos = contaLinhas("modelos.txt");
-    string* marcas = nullptr;
-    marcas = new string[tamanho_marca];
-    string* modelos = nullptr;
-    modelos = new string[tamanho_modelos];
     string nome= " ";
     int et = (rand() % (8 - 3 + 1)) + 3;  //[8,3]
     const int NUM_ETS = et;
     ET* oficina = new ET[NUM_ETS];
-    //Carro* garagem = new Carro[50];
-    //garagem[tamanho_marca].marca;
 
     // Inicializar os valores das ET's
+
     for (int i = 0; i < NUM_ETS; i++) {
-        srand(time(NULL) + i);
+        //srand(time(NULL) + i);
         oficina[i].id = i + 1;
         oficina[i].capacidade = (rand() % (5 - 2 + 1)) + 2; //[5,2]
         oficina[i].carros = (rand() % (oficina[i].capacidade + 1)); //[capacidade,0]
         oficina[i].faturacao = 0;
+        oficina[i].responsavel.especializacao = ficheiroRandom("marcas.txt");
 
         // vetor dinamico para os carros reparados
         oficina[i].reparados = new Carro[oficina[i].capacidade];
+        oficina[i].garagem = new Carro[oficina[i].carros];
 
         //inicializar valores dos carros
         for (int j = 0; j < oficina[i].carros; j++) {
-            srand(time(NULL) + j);
-            oficina[i].responsavel.especializacao = ficheiroRandom(marcas, "marcas.txt");
-            modelos[i] = ficheiroRandom(modelos, "modelos.txt");
+            oficina[i].garagem[j].marca = oficina[i].responsavel.especializacao;
+            oficina[i].garagem[j].modelo = ficheiroRandom("modelos.txt");
+
         }
     }
 
     // Imprimir os dados das ETs
     for (int i = 0; i < NUM_ETS; i++) {
+        //for (int j = 0; j <= i; j++) {
             cout << "ET " << oficina[i].id << "  ";
             cout << "| Responsavel: ";
             getline(cin, nome);
@@ -76,15 +70,19 @@ int main() {
             cout << endl;
             for (int j = 0; j < oficina[i].carros; j++) {
                 cout << "Carro: ";
-                cout << "ID:   | " << oficina[i].responsavel.especializacao << " - " << modelos[i] << endl;
+                cout << "ID:   | " << oficina[i].garagem[j].marca <<"-"<< oficina[i].garagem[j].modelo << endl;
             }
             cout << endl;
+       // }
     }
 
-    // Liberar a memoria alocada para os vetores dinamicos de carros reparados
+    // Liberar a memoria alocada para os vetores dinamicos
     for (int i = 0; i < NUM_ETS; i++) {
         delete[] oficina[i].reparados;
-    }return 0;
+        delete[] oficina[i].garagem;
+
+    }
+    return 0;
 }
 
 
