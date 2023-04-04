50d73068660ddf318f5f5e16088662d3094a41f9
NcpdArt <128602738+NcpdArt@users.noreply.github.com>
2023-04-04 19:49:22 +0100

    Update structs.h

--- PrimeiroProjEDA/structs.h
+++ PrimeiroProjEDA/structs.h
@@ -21,6 +21,7 @@ struct Mecanico {
 struct ET {
     int id; // ID da estacao de trabalho
     Mecanico responsavel; // mecanico responsavel pela estacao de trabalho
+    Carro* garagem;  //apontador para os carros que estao com cada mecanico;
     Carro *reparados; // apontador para o vetor dinamico de carros reparados
     int capacidade; // capacidade maxima da estacao de trabalho
     float faturacao; // faturacao da estacao de trabalho
